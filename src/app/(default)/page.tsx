fbe67c29c25de14a3240a119c6f3df54e0371f60
Prashant Singh <prashant.s2922@gmail.com>
2025-09-11 10:39:47 +0530

    chore: cleanup

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,29 +1,6 @@
-"use client";
-
 import { Hero } from "@/components/feature";
-import React, { useEffect } from "react";
 
 const Home: React.FC = () => {
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch(
-        "https://ici-fest-skit.vercel.app/api/v1/schedule?type=all&page=1&limit=20",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            TESTING_SECRET: "5eGW9SH4dakBXFLT2U5zDl4F8fPiAwPf",
-          },
-          cache:"no-cache"
-        },
-      );
-      const data = await res.json();
-      console.log(data);
-    }
-
-    fetchData();
-  }, []);
-
   return (
     <main className="px-[50px]">
       <Hero />
